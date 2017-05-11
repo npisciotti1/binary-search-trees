8c1973158af1a3e7c6567e0f2e56b90d33a56508
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-11 15:58:13 -0700

    depth-first.js added

---
+++ depth-first.js
@@ -0,0 +1,29 @@
+//Pre-order traversal.
+
+//I've added the constructor pattern here to help clarify the traversal methods
+//below and how they work.
+
+
+function Node(val) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
+}
+
+function preOrder(node) {
+  console.log(node.value);
+  if(node.left) preOrder(node.left);
+  if(node.right) preOrder(node.left);
+}
+
+function inOrder(node) {
+  if(node.left) inOrder(node.left);
+  console.log(node.value);
+  if(node.right) inOrder(node.right);
+}
+
+function postOrder(node) {
+  if(node.right) postOrder(node.right);
+  console.log(node.value);
+  if(node.left) postOrder(node.left);
+}
