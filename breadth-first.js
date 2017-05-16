1c0ffa6fdea73a205e4f9a88e8b2bd6360a6d6d0
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-15 19:54:44 -0700

    scaffold for breadth-first solution

--- breadth-first.js
+++ breadth-first.js
@@ -1,1 +1,19 @@
+//This method below is a breadth-first traversal of a BST
 
+
+function Node(val) {
+  this.value = val;
+  this.left = null;
+  this.right = null;
+  this.parent = null;
+}
+
+function breadthFirstSearch(root) {
+  var queue = [];
+
+  while(queue.length > 0) {
+    var node = queue[0];
+    if(node.left) queue.push(node.left);
+    if(node.right) queue.push(node.right)
+  }
+}
