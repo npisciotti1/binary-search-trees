1f268ed6dfc6e8b01e9a093ccfa684cd0dc9f984
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-15 20:08:59 -0700

    breadth-first solution working

--- breadth-first.js
+++ breadth-first.js
@@ -9,11 +9,18 @@ function Node(val) {
 }
 
 function breadthFirstSearch(root) {
+  if(!root) return;
+
   var queue = [];
+  queue.push(root);
 
   while(queue.length > 0) {
     var node = queue[0];
+    
+    console.log(node.value);
     if(node.left) queue.push(node.left);
     if(node.right) queue.push(node.right)
+
+    queue.shift();
   }
 }
