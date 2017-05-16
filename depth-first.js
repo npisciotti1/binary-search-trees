8237dfa68eb3690a736ae337aa2ceab4d16f81dc
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-05-15 19:45:26 -0700

    depth-first traversal methods

--- depth-first.js
+++ depth-first.js
@@ -1,4 +1,4 @@
-//Methods for Pre-Order, In-Order, and Post-Order traversal of a BST.
+//Methods for Pre-Order, In-Order, and Post-Order "Depth-first" traversal of a BST.
 
 //I've added the constructor pattern here to help clarify the traversal methods
 //below and how they work.
